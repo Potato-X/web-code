c6751d3b266c2809a23c54f58f952e6ad0019393
winner <921876203@qq.com>
2025-12-05 17:27:31 +0800

    add:新增editor文案输入时，词法提示功能

--- src/components/editor/index.vue
+++ src/components/editor/index.vue
@@ -1,27 +1,61 @@
 <template>
-    <div ref="editorContainerRef" class="editor-container">
+    <div ref="editorContainerRef" id="editor-container" class="editor-container">
 
     </div>
 </template>
 
 <script setup lang="ts">
 import * as monaco from 'monaco-editor';
+import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import TsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 import { onMounted, ref } from 'vue';
-
 const editorContainerRef = ref<HTMLElement>()
 const editor = ref<monaco.editor.IStandaloneCodeEditor>()
 onMounted(() => {
     if (editorContainerRef.value) {
-        editor.value = monaco.editor.create(editorContainerRef.value, {
-            value: [
-                'function hello() {',
-                '  console.log("Hello Monaco");',
-                '}'
-            ].join('\n'),
-            language: 'javascript',
-            theme: 'vs-dark',          // 或 'vs'（浅色）、'vs-dark'（深色）等
-            automaticLayout: true      // 窗口 / 容器尺寸变化时自动调整编辑器大小
+        self.MonacoEnvironment = {
+            getWorker(_moduleId: string, label: string) {
+                if (label === "typescript" || label === "javascript") {
+                    return new TsWorker();
+                }
+                return new EditorWorker();
+            }
+        };
+        editor.value = monaco.editor.create(document.getElementById("editor-container")!, {
+            value: "",
+            language: "javascript",
+            theme: "vs-dark",
         });
+        // monaco.languages.registerCompletionItemProvider('javascript', {
+        //     provideCompletionItems(model, position, context, token) {
+        //         const word = model.getWordUntilPosition(position); // 光标处的单词
+        //         const range = {
+        //             startLineNumber: position.lineNumber,
+        //             startColumn: word.startColumn,
+        //             endLineNumber: position.lineNumber,
+        //             endColumn: word.endColumn
+        //         };
+        //         return {
+        //             suggestions: [
+        //                 {
+        //                     label: 'myFunc',
+        //                     kind: monaco.languages.CompletionItemKind.Function,
+        //                     insertText: 'myFunc()',
+        //                     range: range,              // ⭐ 必须要有
+        //                     documentation: '这是一个自定义函数提示'
+        //                 },
+        //                 {
+        //                     label: 'mySnippet',
+        //                     kind: monaco.languages.CompletionItemKind.Snippet,
+        //                     insertText: 'console.log(${1:value})',
+        //                     range: range,               // ⭐ 必须要有
+        //                     insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        //                     documentation: '一个 snippet 示例'
+        //                 }
+        //             ]
+        //         };
+        //     },
+        // });
     }
 
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { defineConfig } from "vite";
+import monacoEditorEsmPlugin from "vite-plugin-monaco-editor-esm";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), monacoEditorEsmPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
